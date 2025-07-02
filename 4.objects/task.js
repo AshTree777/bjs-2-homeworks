9d4064c439c6680e97e1427632f537709e174f0e
Sergey Ivanov <AshTree777@yandex.ru>
2025-07-02 21:00:56 +0300

    Задание 4.objects выполнено полностью

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (length.marksToAdd !== 0) {
+        this.marks.push(...marksToAdd);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.hasOwnProperty('marks') && (this.marks.length !== 0)) {
+        return this.marks.reduce((acc, element) => (acc + element)) / this.marks.length;
+    }
+    return 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    if (this.marks.length === 0) {
+        delete this.marks;
+        delete this.subject;
+        this.exclude = reason;
+    }
 }
